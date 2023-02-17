49895864738a2adf26e422cdf380d1e7b08993f3
chetan <chetanbhagat9168@gmail.com>
2023-02-17 23:11:21 +0530

    update

--- Java Script/common.js
+++ Java Script/common.js
@@ -10,7 +10,7 @@
 //    }
 // })
 function downloadresume(){
-    window.open("https://drive.google.com/drive/folders/1Y2SeLoGQG90y3e-DMdpMWb_BXFOapuuJ?usp=sharing");
+    window.open("https://export-download.canva.com/7XU8E/DAFa2X7XU8E/4/0-47195213441.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHKNGJLC2J7OGJ6Q%2F20230217%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230217T172553Z&X-Amz-Expires=5235&X-Amz-Signature=74219797c88eca3b63bef0f83dfbd405612d81a9916fc47d41efe57eaa2b72da&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27Designation%2520Analyst%2520Role%2520%26%2520Responsibility-%2520%25E2%2596%25AA%2520Data%2520Visualization%2520and%2520data%2520milling%2520by%2520using%2520different%2520data%2520visualization%2520tools.%2520Provide%2520technical%2520consulting%2520to%2520the%2520customer%2520category%2520managers%2520around%2520th%2520industry%2520best%2520pra.pdf&response-expires=Fri%2C%2017%20Feb%202023%2018%3A53%3A08%20GMT");
 }
 let theme=document.querySelector(".theme");
 let theme1= document.querySelector(".theme1");
